283784238d9a0f1da4a4f258d5ffb3fbb0590ebe
SuRu <su33.dev@gmail.com>
2017-10-11 11:53:49 -0500

    arrays examples

---
+++ Java/New/ArrayAdd.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class ArrayAdd {
+
+    public static void main(String[] args) {
+
+        int[] array1 = { 1, 2, 3 };
+        int[] array2 = { 4, 5, 6 };
+        int[] array1and2 = new int[array1.length + array2.length];
+
+        System.arraycopy(array1, 0, array1and2, 0, array1.length);
+        System.arraycopy(array2, 0, array1and2, array1.length, array2.length);
+
+        System.out.println(Arrays.toString(array1and2));
+
+    }
+
+}
---
+++ Java/New/Median.java
@@ -0,0 +1,23 @@
+import java.security.Principal;
+import java.util.Arrays;
+
+public class Median {
+
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 4, 5, 6, 7 };
+        System.out.println("Median: " + median(a));
+    }
+
+    private static double median(int[] numArray) {
+        Arrays.sort(numArray);
+        int middle = ((numArray.length) / 2);
+
+        if (numArray.length % 2 == 0) {
+            int medianA = numArray[middle];
+            int medianB = numArray[middle - 1];
+            return (medianA + medianB) / 2.0;
+        } else {
+            return numArray[middle];
+        }
+    }
+}
